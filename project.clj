19f1792ac27ca14d7616cc6fa406ac84c46428b0
Mingli <mingli.yuan@gmail.com>
2013-01-07 17:45:25 +0800

    add repo for stan-cn-nlp

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject com.guokr.nlp/stan-cn-nlp "0.1.0"
-    :description "A wrapper of Stanford NLP packages for Simplified Chinese users"
+(defproject com.guokr.nlp/clj-cn-nlp "0.1.0"
+    :description "A clojure wrapper of Stanford NLP packages for Simplified Chinese users"
     :dependencies [[org.clojure/clojure "1.4.0"]
                    [edu.stanford.nlp/stanford-corenlp "1.3.3"]
                    [com.guokr/stan-cn-nlp "0.0.1"]]
@@ -13,6 +13,8 @@
     :compile-path "targets/classes"
     :target-path "targets/"
     :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
-    :jar-name "clj-cn-nlp.jar"
 
-    :jvm-opts ["-Xmx2g"])
+    :jvm-opts ["-Xmx2g"]
+
+    :repositories [["sonatype" "http://oss.sonatype.org/content/repositories/releases"]])
+
